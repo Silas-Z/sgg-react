aa6038e42b0e6e8c6ed211212eb65778ff2202b2
Silas <123zsl@163.com>
2021-11-06 10:42:46 +0800

    修改了组件，嵌入了bootstrap

--- react-staging/src/components/list/list.jsx
+++ react-staging/src/components/list/list.jsx
@@ -5,32 +5,32 @@ export default class List extends Component {
         return (
             <div className="row">
                 <div className="card">
-                    <a href="https://github.com/reactjs" target="_blank">
-                        <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{ width: 100 }} />
+                    <a href="https://github.com/reactjs" rel="noreferrer" target="_blank">
+                        <img alt="avatar" src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{ width: "100px" }} />
                     </a>
                     <p className="card-text">reactjs</p>
                 </div>
                 <div className="card">
-                    <a href="https://github.com/reactjs" target="_blank">
-                        <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{ width: 100 }} />
+                    <a href="https://github.com/reactjs" rel="noreferrer" target="_blank">
+                        <img alt="avatar" src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{ width: "100px" }} />
                     </a>
                     <p className="card-text">reactjs</p>
                 </div>
                 <div className="card">
-                    <a href="https://github.com/reactjs" target="_blank">
-                        <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{ width: 100 }} />
+                    <a href="https://github.com/reactjs" rel="noreferrer" target="_blank">
+                        <img alt="avatar" src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{ width: "100px" }} />
                     </a>
                     <p className="card-text">reactjs</p>
                 </div>
                 <div className="card">
-                    <a href="https://github.com/reactjs" target="_blank">
-                        <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{ width: 100 }} />
+                    <a href="https://github.com/reactjs" rel="noreferrer" target="_blank">
+                        <img alt="avatar" src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{ width: "100px" }} />
                     </a>
                     <p className="card-text">reactjs</p>
                 </div>
                 <div className="card">
-                    <a href="https://github.com/reactjs" target="_blank">
-                        <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{ width: 100 }} />
+                    <a href="https://github.com/reactjs" rel="noreferrer" target="_blank">
+                        <img alt="avatar" src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{ width: "100px" }} />
                     </a>
                     <p className="card-text">reactjs</p>
                 </div>
