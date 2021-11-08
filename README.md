f525c4521bae1554780a6205061ff735a1488300
Silas <123zsl@163.com>
2021-11-08 12:41:10 +0800

    添加 navlink

--- README.md
+++ README.md
@@ -4,5 +4,6 @@
 
 问题记录
 
-路由 提示：Error: A <Route> is only ever to be used as the child of <Routes> element, never rendered directly. Please wrap your <Route> in a <Routes>.
+路由 提示：Error: A <Route> is only ever to be used as the child of <Routes> element, never rendered directly. Please wrap your <Route> in a <Routes>, 添加后也无法导航
 示范视频没有包裹 <Routes> 也可以正常显示
+修改插件版本问题解决 
--- react-staging/08-router-basic/App.js
+++ react-staging/08-router-basic/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { NavLink, Route, BrowserRouter } from 'react-router-dom'
+import { Link, Route, BrowserRouter } from 'react-router-dom'
 
 import Home from './components/Home'
 import About from './components/About'
@@ -19,8 +19,8 @@ export default class App extends Component {
                     <div className="row">
                         <div className="col-xs-2 col-xs-offset-2">
                             <div className="list-group">
-                                <NavLink to="/home" className="list-group-item">Home</NavLink>
-                                <NavLink to="/about" className="list-group-item">About</NavLink>
+                                <Link to="/home" className="list-group-item">Home</Link>
+                                <Link to="/about" className="list-group-item">About</Link>
                                 {/* <a className="list-group-item" href="./about.html">About</a>
                             <a className="list-group-item active" href="./home.html">Home</a> */}
                             </div>
@@ -31,6 +31,8 @@ export default class App extends Component {
                                     
                                         <Route path="/home" component={Home} />
                                         <Route path="/about" component={About} />
+                                    <Home />
+                                    <h3>我是Home的内容</h3>
                                 </div>
                             </div>
                         </div>
